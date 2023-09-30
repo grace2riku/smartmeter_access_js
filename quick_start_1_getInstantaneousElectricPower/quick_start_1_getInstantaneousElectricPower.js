0bcbc32988fdc91f4c6f07fb57d60d2470f5982d
Koji Abe <gracehime@gmail.com>
2023-09-30 22:34:46 +0900

    シリアル、スマートメータのID・パスワードをテキストファイルから読み込むように変更

--- quick_start_1_getInstantaneousElectricPower/quick_start_1_getInstantaneousElectricPower.js
+++ quick_start_1_getInstantaneousElectricPower/quick_start_1_getInstantaneousElectricPower.js
@@ -1,11 +1,32 @@
 // node-wisunrb をロードし、`Wisunrb` コンストラクタオブジェクトを取得
 const Wisunrb = require('node-wisunrb');
 
+const fs = require('fs');
+
+// ファイルを読み込む関数
+function readConfig(filePath) {
+    const content = fs.readFileSync(filePath, 'utf8');
+    const lines = content.split('\n');
+    const config = {};
+
+    lines.forEach(line => {
+        const [key, value] = line.split(':');
+        if (key && value) {
+            config[key.trim()] = value.trim();
+        }
+    });
+
+    return config;
+}
+
+// config.txtから設定を読み込む
+const config = readConfig('../config.txt');
+
 // Wisunrb オブジェクトを生成
 const wisunrb = new Wisunrb({
-    path: 'COM6', // シリアルポートのパス
-    id: '0123456789ABCDEF0123456789ABCDEF', // 電力スマートメーターの ID
-    password: '0123456789AB' // 電力スマートメーターのパスワード
+    path: config.path,
+    id: config.id,
+    password: config.password
 });
 
 (async () => {
